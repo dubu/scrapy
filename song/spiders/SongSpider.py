73348be3465f4f582a40557de9c788779fd2c505
dubu <kozazz@hanmail.net>
2015-02-13 13:45:57 +0900

    한글 유니코드 수정

--- song/spiders/SongSpider.py
+++ song/spiders/SongSpider.py
@@ -38,11 +38,11 @@ class SongSpider(CrawlSpider):
             if 0 == len(cand):    continue
             attr = cand.extract()[0]
             print attr
-            if attr == '국적':
+            if attr == u'국적':
                 cand = tr.xpath("td[2]/span[@class='role']/a[1]/text()[1]")
                 if 0 < len(cand):
                     songItem['nationality'] = cand.extract()[0]
-            if attr == '출생':
+            if attr == u'출생':
                 cand = tr.xpath('td[2]/text()[1]')
                 if 0 < len(cand):
                     songItem['birthday'] = cand.extract()[0]
@@ -51,15 +51,15 @@ class SongSpider(CrawlSpider):
                 for c in cand.xpath('a'):
                     birthplace += ' ' + c.xpath('text()[1]').extract()[0]
                 songItem['birthplace'] = birthplace.strip()
-            if attr == '키':
+            if attr == u'키':
                 cand = tr.xpath('td[2]/text()[1]')
                 if 0 < len(cand):
                     songItem['height'] = cand.extract()[0]
-            if attr == '체중':
+            if attr == u'체중':
                 cand = tr.xpath('td[2]/text()[1]')
                 if 0 < len(cand):
                     songItem['weight'] = cand.extract()[0]
-            if attr == '포지션':
+            if attr == u'포지션':
                 cand = tr.xpath("td[2]/span[@class='role']")
                 positionCand = ''
                 t = cand.xpath('text()[1]')
@@ -69,11 +69,11 @@ class SongSpider(CrawlSpider):
                 for p in cand.xpath('a'):
                     positionCand += ' ' + p.xpath('text()[1]').extract()[0].strip()
                 songItem['position'] = positionCand.strip()
-            if attr == '현 소속팀':
+            if attr == u'현 소속팀':
                 cand = tr.xpath("td[2]/span[@class='org']/a[1]/text()[1]")
                 if 0 < len(cand):
                     songItem['currentClub'] = cand.extract()[0]
-            if attr == '등번호':
+            if attr == u'등번호':
                 cand = tr.xpath('td[2]/text()[1]')
                 if 0 < len(cand):
                     songItem['number'] = cand.extract()[0]
