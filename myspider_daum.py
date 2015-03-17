dd6ef111814c3dc0a717c6da8a70064ab23d5643
dubu <kozazz@hanmail.net>
2015-03-17 03:11:24 -0400

    modify daum xpath

--- myspider_daum.py
+++ myspider_daum.py
@@ -18,7 +18,12 @@ class BlogSpider(Spider):
         sel = Selector(response)
         #print sel.xpath(/html/body/center/table/tbody/tr/td[2]/table/tbody/tr/td/form/table[2]/tbody/tr/td[3]
         print '##############'
-        print sel.xpath('/html/body/div[3]/main/article/div/div/div/div/div/ul/li')
+        #print sel.xpath('/html/body/div[3]/main/article/div/div/div/div/div/ul/li')
         #return [Post(title=e.extract()) for e in response.css("h2 a::text")]
-        return [Post(title=e.extract()) for e in response.css("h2 a::text")]
+	# real time issute
+	#for e in sel.xpath('/html/body/div[3]/main/article/div/div/div/div[2]/div/ol/li[6]/div/div/span[2]/a/text()[1]').extract()[0]:
+	for e in sel.xpath('/html/body/div[3]/main/article/div/div/div/div[2]/div/ol/li'):
+		print e.xpath('div/div/span[2]/a/text()[1]').extract()[0]
+        return [Post(title=e) for e in sel.xpath('/html/body/div[3]/main/article/div/div/div/div[2]/div/ol/li[6]/div/div/span[2]/a/text()[1]').extract()[0]]
+        #return [Post(title=e.xpath('/div/div/span[2]/a/text()[1]') ) for e in sel.xpath('/html/body/div[3]/main/article/div/div/div/div[2]/div/ol/li')]
     
